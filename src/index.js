011853988fe0e807c2e6ff57a40f614ea68710a7
unknown <salarbutt321@gmail.com>
2023-10-16 13:17:58 +0500

    path update

--- src/index.js
+++ src/index.js
@@ -4,14 +4,14 @@ import './index.css';
 import './Screens/home.css'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter basename='/'>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode> 
 );
 
